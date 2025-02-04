3dfe419e943d08dd1bd1928b70ff032b02b2d46e
YAMRAAJ0 <tiosuthar@gmail.com>
2025-02-04 19:15:42 +0530

    img

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -2,61 +2,33 @@
 
 import { useRouter } from "next/navigation";
 import { useState } from "react";
+import { links, RoleBased } from "./Navbardata";
+interface HeaderProps {
+  setMenuOpen: (value: boolean) => void; // Define function type
+}
 
-const Navbar = () => {
+const Header: React.FC<HeaderProps> = ({ setMenuOpen }) => {
   const router = useRouter(); // Use useRouter instead of import router
   const [, setLoading] = useState(false);
   const [, setIsOpen] = useState(false);
 
-  const links = [
-    { icon: "/images/linux1.png", label: "Linux", details: "Linux details" },
-    { icon: "/images/docker.webp", label: "Docker", details: "Docker details" },
-    { icon: "/images/Python.webp", label: "Python", details: "Python details" },
-    { icon: "/images/Github.png", label: "GitHub", details: "GitHub details" },
-    { icon: "/images/Terraform.png", label: "Terraform", details: "Terraform details" },
-    { icon: "/images/Kubernetes.webp", label: "Kubernetes", details: "Kubernetes details" },
-    { icon: "/images/Java.png", label: "Java", details: "Java details" },
-    { icon: "/images/ChatGPT.webp", label: "ChatGPT", details: "ChatGPT details" },
-    { icon: "/images/Ansible.png", label: "Ansible", details: "Ansible details" },
-    { icon: "/images/AWS.webp", label: "AWS", details: "AWS details" },
-    { icon: "/images/Jenkins.png", label: "Jenkins", details: "Jenkins details" },
-    { icon: "/images/MySQL.png", label: "MySQL", details: "MySQL details" },
-    { icon: "/images/MEAN & MERN.png", label: "MEAN & MERN", details: "MEAN & MERN details" },
-    { icon: "/images/Azure.png", label: "Azure", details: "Azure details" },
-    { icon: "/images/Cloud%20Computing.png", label: "Cloud Computing", details: "Cloud Computing details" },
-    { icon: "/images/DevOps.png", label: "DevOps", details: "DevOps details" },
-    { icon: "/images/GCP.png", label: "GCP", details: "GCP details" },
-    { icon: "/images/CI/CD.png", label: "CI/CD", details: "CI/CD details" },
-    { icon: "/images/Backstage.png", label: "Backstage", details: "Backstage details" },
-    { icon: "/images/AI/ML.png", label: "AI/ML", details: "AI/ML details" },
-    { icon: "/images/AppSecurity.png", label: "AppSecurity", details: "AppSecurity details" },
-    { icon: "/images/DS%2FALGO.png", label: "DS/ALGO", details: "DS/ALGO details" },
-  ];
-
-  const RoleBased = [
-    { icon: "/images/image.png", label: "System Administrator", details: "Linux details" },
-    { icon: "/images/image.png", label: "Kubernetes Administrator", details: "Linux details" },
-    { icon: "/images/image.png", label: "Site Reliability Engineer", details: "Linux details" },
-    { icon: "/images/image.png", label: "Kubernetes Developer", details: "Linux details" },
-    { icon: "/images/image.png", label: "Platform Engineer", details: "Linux details" },
-    { icon: "/images/image.png", label: "DevOps Engineer", details: "Linux details" },
-    { icon: "/images/image.png", label: "Cloud Engineer", details: "Linux details" },
-  ];
 
 
   const handleNavigate = async (courseName: string) => {
     setLoading(true);
-
+  
     if (courseName === "Internship") {
       router.push("/internship");
     } else {
       const encodedText = encodeURIComponent(courseName);
       router.push(`/coursecontent?message=${encodedText}`);
     }
-
+  
     setLoading(false);
     setIsOpen(false);
+    setMenuOpen(false); 
   };
+  
 
   return (
     <div className="bg-black w-full  justify-center px-40 border-b border-gray-400/30 flex flex-col lg:flex-row divide-y lg:divide-y-0 lg:divide-x divide-gray-400/30">
@@ -105,4 +77,4 @@ const Navbar = () => {
   );
 };
 
-export default Navbar;
+export default Header;
